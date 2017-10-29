8ad9bc937566c010d7454075f8447ce96b02f279
Aryan Gupta <kuantum.freak@gmail.com>
2017-10-29 17:28:54 -0400

    Add calls to 32-bit protected mode

--- boot/src/bootloader.asm
+++ boot/src/bootloader.asm
@@ -6,7 +6,11 @@ jmp boot_start
 
 %include "src/print_16.asm"
 %include "src/load_kernal.asm"
+%include "src/gd_table.asm"
+%include "src/switch_32.asm"
+%include "src/print_32.asm"
 
+[BITS 16]
 boot_start:
 	mov bp, 0x8000 ; set the stack
     mov sp, bp
@@ -40,19 +44,40 @@ boot_start:
 	mov si, KERNAL_LOADED
 	call print_str_16
 	call print_nl_16
+
+	
+	mov si, SWITCH_32
+	call print_str_16
+	call print_nl_16
+
+	call switch_to_pm
+	
+	jmp boot_end ; wont run
+	
+	
+[BITS 32]
+pm_mode_start:
+	mov esi, IN_32_MODE
+	call print_str_32
+	call print_nl_32
+	
+	;call KERNAL_OFFSET
 	
 	jmp boot_end
 	
 
 IN_16_MODE: db 'In 16-bit mode', 0
 IN_32_MODE: db 'In 32-bit mode', 0
-PRESS_ENTER: db 'Press enter to continue booting', 0
-ENTER_PRESSED: db 'Enter pressed, booting into OS-2...', 0
-WELCOME: db 'Welcome to OS-2', 0
+;PRESS_ENTER: db 'Press enter to continue booting', 0
+;ENTER_PRESSED: db 'Enter pressed, booting into OS-2...', 0
+;WELCOME: db 'Welcome to OS-2', 0
 KERNAL_LOADED: db 'Kernel loaded sucessfully', 0
+SWITCH_32: db 'Switching to 32-bit Mode', 0
 	
+
 boot_end:
 	jmp $
-	times 510-($-$$) db 0
-	dw 0xaa55
+
+times 510-($-$$) db 0
+dw 0xaa55
 	
