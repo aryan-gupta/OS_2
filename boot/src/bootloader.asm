96f77ca22aa3c86d9bd6b4527dcce2c7bd29e3ce
Aryan Gupta <kuantum.freak@gmail.com>
2017-10-29 12:45:27 -0400

    Code Bootloader Skeleton
    Add printing of 16 bit mode
    Add booting magic codes
    ...

---
+++ boot/src/bootloader.asm
@@ -0,0 +1,21 @@
+[org 0x7c00]
+jmp boot_start
+
+%include "src/print_16.asm"
+
+boot_start:
+	mov si, IN_16_MODE
+	call print_str_16
+	
+	jmp boot_end
+	
+
+IN_16_MODE: db 'In 16-bit mode', 0
+IN_32_MODE: db 'In 32-bit mode', 0
+WELCOME: db 'Welcome to OS-2', 0
+	
+boot_end:
+	jmp $
+	times 510-($-$$) db 0
+	dw 0xaa55
+	
