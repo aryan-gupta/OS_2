c500aa796dde985c4d7a73f76aa28fdda69546c7
Aryan Gupta <kuantum.freak@gmail.com>
2017-11-08 15:27:37 -0500

    Change bootloader to reflect new FAT32 bootloader

--- boot/src/bootloader.asm
+++ boot/src/bootloader.asm
@@ -43,6 +43,14 @@ SystemID              db  "FAT32   "
 
 [BITS 16]
 boot_start:
+	; set all the segment registors
+	cli
+	mov ax, 0x7c0
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
 	mov bp, 0x8000 ; set the stack
     mov sp, bp
 	
@@ -50,63 +58,34 @@ boot_start:
 	call hide_cursor_16
 	call reset_cursor_16
 	
-	mov si, IN_16_MODE
+	mov si, ENTRY_MESSAGE
 	call print_str_16
 	call print_nl_16
 	
-	mov si, WELCOME
+	mov si, CALL_S2_MESSAGE
 	call print_str_16
 	call print_nl_16
 	
-	mov si, PRESS_ENTER
+	call load_stage2
+	
+	mov si, START_S2_MESSAGE
 	call print_str_16
 	call print_nl_16
 	
-	mov bh, 0x1c ; http://www.fountainware.com/EXPL/bios_key_codes.htm
-	call wait_for_key
-	
-	mov si, ENTER_PRESSED
-	call print_str_16
-	call print_nl_16
-	
-	call load_kernal
-	
-	mov si, KERNAL_LOADED
-	call print_str_16
-	call print_nl_16
-
-	
-	mov si, SWITCH_32
-	call print_str_16
-	call print_nl_16
-
-	call switch_to_pm
+	call stage2_start_addr
 	
 	jmp boot_end ; wont run
 	
-	
-[BITS 32]
-pm_mode_start:
-	mov esi, IN_32_MODE
-	call print_str_32
-	call print_nl_32
-	
-	;call KERNAL_OFFSET
-	
-	jmp boot_end
-	
 
-IN_16_MODE: db 'In 16-bit mode', 0
-IN_32_MODE: db 'In 32-bit mode', 0
-;PRESS_ENTER: db 'Press enter to continue booting', 0
-;ENTER_PRESSED: db 'Enter pressed, booting into OS-2...', 0
-;WELCOME: db 'Welcome to OS-2', 0
-KERNAL_LOADED: db 'Kernel loaded sucessfully', 0
-SWITCH_32: db 'Switching to 32-bit Mode', 0
-	
+ENTRY_MESSAGE db 'Welcome to OS 2...', 0
+LOAD_S2_MESSAGE db 'Loading stage 2...', 0
+START_S2_MESSAGE db 'Starting stage 2...', 0
+
+stage2_start_addr equ 0x10_000
 
 boot_end:
-	jmp $
+	cli
+	hlt
 
 times 510-($-$$) db 0
 dw 0xaa55
